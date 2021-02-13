6b0c602ab51f8b8a4572a647d8f30e7fc26ad4e0
Andre Hochi <andrehochiwork@gmail.com>
2021-02-13 22:45:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,6 +24,21 @@ def monopoly_with_second_tier
   bash_hash
 end
 
+def monopoly_with_second_tier
+  bash_hash = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {
+        
+      },
+      :names => {
+        
+      }
+    }
+  }
+  bash_hash
+end
+
 def monopoly_with_third_tier
   bash_hash = {
     :railroads => {
