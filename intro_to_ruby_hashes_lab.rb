40417bc624cdaffd3417a747076d252a13fda2d3
Andre Hochi <andrehochiwork@gmail.com>
2021-02-13 22:44:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,17 +25,18 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  bash_hash = {:railroads => {
-    :pieces => 4,
-    :rent_in_dollars => {
-    }
-      :one_piece_owned => 25,
-      :two_pieces_owned => 50,
-      :three_pieces_owned => 100,
-      :four_pieces_owned => 200
-    },
-    :names => {
+  bash_hash = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
+      :names => {
       
+      }
     }
   }
   bash_hash
@@ -60,6 +61,6 @@ def monopoly_with_fourth_tier
       :shortline_railroad => {
         :mortgage_value => 800}
     }
-  }}
+  }
   bash_hash
 end
